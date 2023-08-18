ccd376ba3ed40e668561a72c5642e3e62a9029c5
alexkinielsen <66766064+alexkinielsen@users.noreply.github.com>
2023-08-17 22:40:53 -0600

    Develop (#27)
    * minor bug fix
    
    * Update dotnet.yaml
    
    * Update dotnet.yaml
    
    * Update dotnet.yaml
    
    * Add vote counts to simulation
    
    * Update dotnet.yaml
    
    * Update dotnet.yaml (#19)
    
    * Update dotnet.yaml
    
    * Alexkinielsen patch 4 (#22)
    
    * Develop (#20)
    
    * minor bug fix
    
    * Update dotnet.yaml
    
    * Update dotnet.yaml
    
    * Update dotnet.yaml
    
    * Add vote counts to simulation
    
    * Update dotnet.yaml
    
    * Update dotnet.yaml (#19)
    
    * Develop (#21)
    
    * minor bug fix
    
    * Update dotnet.yaml
    
    * Update dotnet.yaml
    
    * Update dotnet.yaml
    
    * Add vote counts to simulation
    
    * Update dotnet.yaml
    
    * Update dotnet.yaml (#19)
    
    * Update dotnet.yaml
    
    * Update dotnet.yaml
    
    * Update dotnet.yaml
    
    * Update dotnet.yaml
    
    * Update dotnet.yaml
    
    * add pages to new app (#26)

---
+++ SimulacrumSharp.MAUI/Models/ApiResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace SimulacrumSharp.MAUI.Models
+{
+    public class ApiResponse
+    {
+        public int StatusCode { get; set; }
+        public string ResponseBody { get; set; }
+    }
+}
