803dc03d9435a0837bf8f3da480aacef4a7df9fc
alexkinielsen <66766064+alexkinielsen@users.noreply.github.com>
2023-08-17 22:38:29 -0600

    add pages to new app (#26)

---
+++ SimulacrumSharp.MAUI/Services/Interfaces/ISimulacrumApiService.cs
@@ -0,0 +1,9 @@
+﻿using SimulacrumSharp.MAUI.Models;
+
+namespace SimulacrumSharp.MAUI.Services.Interfaces
+{
+    public interface ISimulacrumApiService
+    {
+        Task<ApiResponse> Post(string controller, string endpoint, object requestObject);
+    }
+}
