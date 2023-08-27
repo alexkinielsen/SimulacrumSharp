1ac2503c16549edb01a9b868be37fde2d13755e6
alexkinielsen <alexkinielsen@gmail.com>
2023-08-27 15:48:47 -0600

    layout work

---
+++ SimulacrumSharp.Razor/Models/SimulationSelectorModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimulacrumSharp.Razor.Models
+{
+    public class SimulationSelectorModel
+    {
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+        public string Link { get; set; }
+    }
+}
